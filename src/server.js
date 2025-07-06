8c1b4943952107e5bc0b6340e10dd9e4f5896880
Sarthak Vitmal <sarthakvitmal0829@gmail.com>
2025-07-07 01:19:13 +0530

    feat: add cron job for periodic API requests and update dependencies

--- src/server.js
+++ src/server.js
@@ -3,12 +3,18 @@ import dotenv from 'dotenv'
 import {connectDB, initDB} from './config/db.js'
 import ratelimiter from './middleware/rateLimiter.js';
 import transactionRoute from './routes/transactionsRoutes.js'
+import job from './config/cron.js';
 
 dotenv.config();
 const app = express();
+if(process.env.NODE_ENV === "production")job.start()
 app.use(ratelimiter)
 app.use(express.json());
 const PORT = process.env.PORT;
+
+app.get("/api/health",(req,res) => {
+    res.status(200).json({status : "ok"})
+})
 
 app.use("/api/transactions",transactionRoute);
 
